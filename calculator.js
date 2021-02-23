dba72ff5a1f3bc786ff979ed18eb220496b14d38
roberterpi <robertpopct@gmail.com>
2021-02-23 18:45:56 +0200

    updates and writing first jacaScript to the calculator

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let runningTotal = 0;
+let buffer = `0`;
+let previousOperator;
+
+const screen = document.querySelector(`.screen`);
+
+function buttonClick (value) {}
+
+function init () {
+  document.querySelector(`.calc-buttons`)
+    .addEventListener(`click`, function(event) {
+      buttonClick(event.target.innerText);
+    })
+}
